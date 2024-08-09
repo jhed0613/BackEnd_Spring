821dc46391c555817effb875d341262601df3ddd
kjh <wogud2437@naver.com>
2024-08-09 15:56:14 +0900

    first commit

---
+++ src/main/java/com/restapi/emp/BackEndRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.restapi.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndRestApiApplication.class, args);
+	}
+}
---
+++ src/main/java/com/restapi/emp/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.restapi.emp.repository;
+
+import com.restapi.emp.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+}
